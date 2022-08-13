d837ea216e2cab69c0641f9644b82bb7074e305f
icaroperetti <icaroperetti50@gmail.com>
2022-08-12 22:51:01 -0300

    refactor: move duplicated code to a helper method

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,6 +1,7 @@
 import { SignUpController } from "./signup";
 import { MissingParamError } from "../errors/missing-param-error";
 
+
 describe("SignUp", () => {
   test("Should return 400 if no name is provided", () => {
     const sut = new SignUpController();
