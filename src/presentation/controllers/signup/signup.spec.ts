a7c296931299f23720196e645851f101e394e3d9
icaroperetti <icaroperetti50@gmail.com>
2022-08-14 23:11:03 -0300

    refactor: move all signup protocols to a single file

--- src/presentation/controllers/signup/signup.spec.ts
+++ src/presentation/controllers/signup/signup.spec.ts
@@ -1,8 +1,15 @@
 import { SignUpController } from "./signup";
-import { MissingParamError, InvalidParamError, ServerError } from "../errors";
-import { EmailValidator } from "../protocols";
-import { AccountModel } from "../../domain/models/account";
-import { AddAccount, AddAccountModel } from "../../domain/usecases/add-account";
+import {
+  MissingParamError,
+  InvalidParamError,
+  ServerError,
+} from "../../errors";
+import {
+  EmailValidator,
+  AccountModel,
+  AddAccount,
+  AddAccountModel,
+} from "./signup-protocols";
 
 const makeEmailValidator = (): EmailValidator => {
   class EmailValidatorStub implements EmailValidator {
@@ -157,7 +164,7 @@ describe("SignUp", () => {
 
   test("Should call AddAccount with correct values", () => {
     const { sut, addAccountStub } = makeSut();
-    const addSpy = jest.spyOn(addAccountStub, "add")
+    const addSpy = jest.spyOn(addAccountStub, "add");
     const httpRequest = {
       body: {
         name: "any_name",
